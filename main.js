f3fe93c43491661e27c79a6b6dd18afca7718559
DevNachoGarcia <igarciariestra@gmail.com>
2023-10-13 17:26:53 -0300

    added dropdowns functionality on FAQ section

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// COUNTDOWN
+
 const countdown = () => {
     const countDate = new Date ('November 10, 2023 00:00:00').getTime();
     const now = new Date().getTime();
@@ -20,3 +22,59 @@ const countdown = () => {
 };
 
 setInterval(countdown, 1000);
+
+
+// FAQ SECTION DROPDOWNS
+
+const questions = [
+    'Why is it free?',
+    'Do I need this if I’ve had my yearly medical check? What is the difference?',
+    'Can I bring another person to the appointment?',
+    'How long does it take?',
+    'What if I have to cancel or if I’m not sure I’ll be able to make it?',
+    'Do I need to bring anything with me?',
+    'What if I don’t have insurance?',
+    'Do the weight loss programs you offer work?',
+    'What’s the catch?',
+    'Will I have to exhaust myself in the gym to make it work?',
+    'Where are you located?'
+];
+
+const list = document.querySelector('.list');
+
+
+questions.forEach(question => {
+    
+    // creating HTML elements
+
+    const item = document.createElement('div');
+    const questionRow = document.createElement('div');
+    const questionText = document.createElement('span');
+    const drop = document.createElement('div');
+    const dropImage = document.createElement('img');
+    const answerRow = document.createElement('div');
+    const answer = document.createElement('p');
+    
+    // classes assignment
+    
+    item.classList.add('list-item');
+    questionRow.classList.add('question-row');
+    questionText.innerText = question;
+    answerRow.classList.add('answer-row');
+    dropImage.setAttribute('src', './assets/images/dropdown.svg');
+    answer.innerHTML = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui, consequuntur dignissimos assumenda, cupiditate repellendus, eum maiores est accusantium laborum soluta debitis unde doloremque doloribus laudantium beatae nulla quibusdam voluptates perferendis.'
+    dropImage.addEventListener('click', () => {
+        answerRow.classList.toggle('show');
+        dropImage.classList.toggle('up-direction');
+    })
+     
+    // Appending childs
+    
+    drop.appendChild(dropImage);
+    questionRow.appendChild(questionText);
+    questionRow.appendChild(drop);
+    answerRow.appendChild(answer);
+    item.appendChild(questionRow);
+    item.appendChild(answerRow)
+    list.appendChild(item);
+})
